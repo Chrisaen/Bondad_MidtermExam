ce7a0a7edadf4927109fbc1fa6e5fbced0eee33d
Chrisaen <145817063+Chrisaen@users.noreply.github.com>
2024-06-20 21:40:38 +0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,16 +6,16 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function getProductsByTheme($theme)
-{
-    // Assuming you have an array of anime products
-    $animeProducts = [
-        ['Naruto', 'One Piece', 'Attack on Titan', 'Frerien', 'Wind Breaker']
-    ];
+    public function welcome()
+    {
+        $products = [
+            ['name' => 'Naruto', 'description' => 'A young ninja who seeks recognition from his peers.', 'price' => 10.00],
+            ['name' => 'One Piece', 'description' => 'A story about pirates seeking the ultimate treasure.', 'price' => 15.00],
+            ['name' => 'Attack on Titan', 'description' => 'Humanity\'s struggle against giant humanoid creatures.', 'price' => 12.00],
+            ['name' => 'My Hero Academia', 'description' => 'A world where nearly everyone has superpowers, known as Quirks.', 'price' => 14.00],
+            ['name' => 'Demon Slayer', 'description' => 'A young boy becomes a demon slayer to avenge his family.', 'price' => 16.00]
+        ];
 
-
-
-    return view('products', ['products' => $animeProducts, 'theme' => $theme]);
-}
-
+        return view('products.welcome', compact('products'));
+    }
 }
